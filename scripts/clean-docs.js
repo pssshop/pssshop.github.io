9bf776ec5b6b0b794914601ce277ea7b860026cf
607ch00 <20735294+607ch00@users.noreply.github.com>
2025-10-06 11:30:50 -0400

    Add favicon

--- scripts/clean-docs.js
+++ scripts/clean-docs.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 const docsDir = path.join(__dirname, '../docs');
-const keepFiles = ['inventory.json'];
+const keepFiles = ['inventory.json', 'favicon.png'];
 
 if (!fs.existsSync(docsDir)) {
   process.exit(0);
